12ab1fb7045fc6a16ce639becdc00670e77906ad
Martijn de Valk <mdevalk@kaliber.net>
2020-03-06 14:19:42 +0100

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
+import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const config = {
+    apiKey: "AIzaSyALnYw32OjvLA5JJmXZtQclC7XgDXr0c7w",
+    authDomain: "magic-mirror-172df.firebaseapp.com",
+    databaseURL: "https://magic-mirror-172df.firebaseio.com",
+    projectId: "magic-mirror-172df",
+    storageBucket: "magic-mirror-172df.appspot.com",
+    messagingSenderId: "164338707361",
+    appId: "1:164338707361:web:a3dd3e304ce5f569ea79e0",
+    measurementId: "G-HYRS8B3DBN"
+  };
+
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
+  var smilingRef = database.ref('message_list/M1jqFxvAlteRcZQDEtB');
+
+  smilingRef.on('smiling', function(snapshot) {
+    console.log(snapshot.val());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
 
 export default App;
+
+
+
